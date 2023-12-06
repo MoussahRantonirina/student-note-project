fa6f4e8ab8592d6f7e58926063d23ce609031ec4
MoussahRantonirina <rantomoussah@gmail.com>
2023-12-06 10:29:24 +0300

    Configuring the sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentComponent } from './component/dashboard/student/student.component';
+import { NoteComponent } from './component/dashboard/note/note.component';
+import { SubjectComponent } from './component/dashboard/subject/subject.component';
 
 const routes: Routes = [
   {path: 'dashboard', children: [
     {path: '', redirectTo:'student', pathMatch: 'full'},
-    {path: 'student', component: StudentComponent}
+    {path: 'student', component: StudentComponent},
+    {path: 'subject', component: SubjectComponent},
+    {path: 'note', component: NoteComponent}
   ]}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { NoteComponent } from './component/dashboard/note/note.component';
+import { SubjectComponent } from './component/dashboard/subject/subject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    SidebarComponent
+    SidebarComponent,
+    NoteComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
